52f4266d55f3431110b164fb0d7f84334fc0290c
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-08-16 22:33:49 -0700

    connecting the Flask API with the webscraper

--- scraping.py
+++ scraping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager as CDM
 from config import chromeDriver
 import pandas as pd
-import time
+import datetime as dt
 
 ### Red Planet Science
    ## - URL: https://redplanetscience.com/
@@ -15,47 +15,80 @@ import time
   ## - URL:  https://galaxyfacts-mars.com/
   ## - Pull statistical information for the web app
 
-def scrape_all():
-   data = dict()
-  ## Red Planet Science
-  ## Website Content Article Structure:
-  ## - tag: div class="row"  :   Each article has it's own div class=row tag seperating them
-  ##   - tag: div class = "list_text"        :       text side of the row, other side has an image
-  ##     - tag: div class = "list_date"      :       location of article date
-  ##     - tag: div class = "content_title"  :       location of article title
-  ##     - tag: div class = "article_teaser_body" :  location of article summary
-    url = "https://redplanetscience.com"
-    browser = Browser("chrome", executable_path=chromeDriver, headless=False)    # Open an automated browser
-    browser.visit(url)                                                           # Go to the first URL
+def mars_news(browser):
+    ## Red Planet Science
+    ## Website Content Article Structure:  (incase of future change)
+    ## - tag: div class="row"  :   Each article has it's own div class=row tag seperating them
+    ##   - tag: div class = "list_text"        :       text side of the row, other side has an image
+    ##     - tag: div class = "list_date"      :       location of article date
+    ##     - tag: div class = "content_title"  :       location of article title
+    ##     - tag: div class = "article_teaser_body" :  location of article summary
+    url = "https://redplanetscience.com"    
+    browser.visit(url)                                                           # Open an automated browser and visit the url
     browser.is_element_present_by_css("div.list_text", wait_time=3)              # wait 3 seconds for the needed tag to appear 
     soup = bs(browser.html, "html.parser")                                       # parse the webpage to be searchable
-    article = soup.find("div", attrs={"class":"row"})
-    news_title = article.find('div', attrs={"class":"content_title"}).get_text()
-    news_date = article.find("div", attrs={"class":"list_date"}).get_text()
-    news_summary = article.find("div", attrs={"class":"article_teaser_body"}).get_text()
-    post = {"title" : news_title, "date" : news_date, "summary" : news_summary}
+    try:
+      article = soup.find("div", attrs={"class":"row"})
+      news_title = article.find('div', attrs={"class":"content_title"}).get_text()
+      news_date = article.find("div", attrs={"class":"list_date"}).get_text()
+      news_summary = article.find("div", attrs={"class":"article_teaser_body"}).get_text()
+      post = {"title" : news_title, "date" : news_date, "summary" : news_summary}
+      return post
+    except AttributeError:
+      return None
+    except Exception as e:
+      print(e) 
+      raise f"Browser Error : {e}"
+    
 
-  ## Space Images
+def mars_images(browser):
+    ## Space Images
     url = "https://spaceimages-mars.com/"
-    browser.visit(url)
-    time.sleep(3)
+    browser.visit(url)                    
+    browser.is_element_present_by_text("FULL IMAGE", wait_time=3)                
     browser.links.find_by_partial_text("FULL IMAGE").click()
     soup = bs(browser.html, "html.parser")
-    image_url = soup.find("img", attrs={"class":"fancybox-image"}).get("src")
-    new_url = url+image_url        # Save this image, it is the new one
+    try:
+      image_url = soup.find("img", attrs={"class":"fancybox-image"}).get("src")
+      new_url = url+image_url        # Save this image, it is the new one
+      return new_url
+    except AttributeError:
+      return None
+    except Exception as e:
+      print(e)  
+      raise f"Browser Error : {e}"
 
-  ## Mars Facts
+
+def mars_facts():
+    ## Mars Facts
     url = "https://galaxyfacts-mars.com/"
-    browser.visit(url)
-    soup = bs(browser.html, "html.parser")
     tables = pd.read_html(url)
-    columns = tables[0].loc[0, :].to_list()
-    columns[0] =  "Description"
-    comparison_df = pd.DataFrame(tables[0].loc[1:, :])
-    comparison_df.columns = columns
-    comparison_df.set_index("Description", inplace=True)
-    comparison_df
-    stats = comparison_df.to_html()
-    browser.quit()
+    try: 
+      columns = tables[0].loc[0, :].to_list()       # getting the column names
+      columns[0] =  "Description"
+      comparison_df = pd.DataFrame(tables[0].loc[1:, :])
+      comparison_df.columns = columns
+      comparison_df.set_index("Description", inplace=True)
+      comparison_df
+      stats = comparison_df.to_html()
+      return stats
+    except AttributeError:
+      return None
+    except Exception as e:
+      print(e)
+      raise f"Import Error: {e}"
+    
 
-    return data 
+def scrape_all():
+    browser = Browser("chrome", executable_path=chromeDriver, headless=True)
+    news = mars_news(browser)
+    news['featured_image'] = mars_images(browser)
+    news['facts'] = mars_facts()
+    news['last_modified'] = dt.datetime.now()
+    browser.quit()
+    return news
+
+
+
+if __name__ == "__main__":
+    print(scrape_all())
